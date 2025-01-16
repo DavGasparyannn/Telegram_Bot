827cc025dfa5f268cc6816f931ad9ea0cc0330f0
Gasparyan Dav <zadref45@gmail.com>
2025-01-16 23:38:53 +0400

    Добавьте файлы проекта.

---
+++ Telegram_Bot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Telegram_Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("7824764223:AAEBQywOoE8-sCyscVMQexcs-3TOd38ozxU");
+            client.StartReceiving(Update, Error);
+            Console.ReadKey();
+        }
+
+
+        private static async Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message != null)
+            {
+                if (message.Text.ToLower().Contains("barev"))
+                {
+                    await client.SendMessage(message.Chat.Id,"Barior brat");
+                    return;
+                }
+            }
+        }
+        private static async Task Error(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
