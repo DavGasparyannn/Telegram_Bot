a562acb49db981ca84661917d9c03a5c9b7efa00
Gasparyan Dav <zadref45@gmail.com>
2025-01-25 01:14:44 +0400

    dont correct downloader from spotify

--- Telegram_Bot/Downloaders/YoutubeDownloader.cs
+++ Telegram_Bot/Downloaders/YoutubeDownloader.cs
@@ -35,6 +35,29 @@ namespace Telegram_Bot.Downloaders
                 Console.WriteLine("Ошибка: " + ex.Message);
             }
         }
-       
+        private static async Task<string> DownloadSongFromYoutubeBySpotify(string songName)
+        {
+            var outputFileName = $"{songName}.mp3".Replace(" ", "_");
+            var outputPath = Path.Combine(Directory.GetCurrentDirectory(), outputFileName);
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "yt-dlp", // Убедись, что yt-dlp доступен в PATH
+                    Arguments = $"-x --audio-format mp3 -o \"{outputPath}\" \"ytsearch:{songName}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+
+            return File.Exists(outputPath) ? outputPath : null;
+        }
+
     }
 }
