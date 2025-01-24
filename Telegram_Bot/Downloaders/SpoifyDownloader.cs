8346d9535ccf7fcd01e04adb5dc090d439e04821
Gasparyan Dav <zadref45@gmail.com>
2025-01-25 01:23:29 +0400

    backup

---
+++ Telegram_Bot/Downloaders/SpoifyDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Telegram_Bot.Downloaders
+{
+    public class SpoifyDownloader
+    {
+        public void DownloadTrack(string spotifyUrl, string downloadPath)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "spotdl",
+                    Arguments = $"\"{spotifyUrl}\" --output \"{downloadPath}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(startInfo))
+                {
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при скачивании трека: {ex.Message}");
+            }
+        }
+    }
+}
